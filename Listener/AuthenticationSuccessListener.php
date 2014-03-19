a3a4dceee053325929b2e036871616543f488a0c
Anas AMEZIANE <aameziane@sii.fr>
2014-03-19 11:07:02 +0100

    Fix merge bug

--- Listener/AuthenticationSuccessListener.php
+++ Listener/AuthenticationSuccessListener.php
@@ -32,7 +32,6 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\Security\Core\Role\SwitchUserRole;
 use Symfony\Component\Security\Core\SecurityContextInterface;
-use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
 use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 use Symfony\Component\Security\Http\Authentication\AuthenticationSuccessHandlerInterface;
 use Symfony\Component\HttpFoundation\Request;
